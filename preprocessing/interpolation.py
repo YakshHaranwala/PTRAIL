7d3ec8de2d1eb89c287ce938e03cf2ac6bdd1b89
YakshHaranwala <yjharanwala@mun.ca>
2021-06-17 18:26:33 -0400

    Changes by Salman

--- preprocessing/interpolation.py
+++ preprocessing/interpolation.py
@@ -78,7 +78,7 @@ class Interpolate:
             -------
                 NumPandasTraj:
                     The dataframe containing enhanced trajectories with interpolated
-                    points
+                    points and sorted according to traj_id and Datetime.
 
             Raises
             ------
@@ -168,8 +168,109 @@ class Interpolate:
                 }
 
     @staticmethod
-    def cubic_interpolation(dataframe):
-        pass
+    def cubic_interpolation(dataframe, distance_threshold):
+        """
+            Use the Cubic Interpolation method to smoothen the trajectory
+            when the distance between 2 consecutive points is greater than
+            the user-specified value.
+
+            Parameters
+            ----------
+                dataframe:
+                    The dataframe consisting of the points of a single trajectory.
+                distance_threshold: float
+                    The maximum distance between 2 consecutive points of a trajectory.
+
+            Returns
+            -------
+                pandas.core.dataframe.DataFrame:
+                    The dataframe with enhanced trajectory points calculated by linear
+                    interpolation method.
+
+            References
+            ----------
+                "Etemad, M., Soares, A., Etemad, E. et al. SWS: an unsupervised trajectory
+                segmentation algorithm based on change detection with interpolation kernels.
+                Geoinformatica (2020)"
+        """
+        df = dataframe.copy()
+        df1 = df[[const.TRAJECTORY_ID, const.DateTime, const.LAT, const.LONG, const.PREV_DIST]]
+        ids_ = df1[const.TRAJECTORY_ID].to_list()
+        dists = dataframe[const.PREV_DIST].fillna(0).to_list()
+        for i in range(len(df)):
+            if dists[i] > distance_threshold:
+                if (dists[i - 1] and dists[i - 2]) and (ids_[i - 2] == ids_[i - 1] == ids_[i]):
+                    vals = ip_help._cubic_helper(dataframe.iloc[i - 2: i + 1])
+
+                elif ids_[i - 1] == ids_[i] == ids_[i + 1]:
+                    vals = ip_help._cubic_helper(dataframe.iloc[i - 1: i + 2])
+
+                df1.loc[(2 * i - 1) / 2] = [dataframe[const.TRAJECTORY_ID].iloc[0], vals['inter_time'],
+                                            vals['inter_x'], vals['inter_y'], 0]
+
+        return df1.sort_index().reset_index(drop=True)
+
+    @staticmethod
+    def cubic_alt(dataframe, distance_threshold):
+        df = dataframe.reset_index().copy()
+        ids_ = list(dataframe.traj_id.value_counts().keys())
+        chunks = [df.loc[df[const.TRAJECTORY_ID] == ids_[i]] for i in range(len(ids_))]
+        results = []
+        for i in range(len(chunks)):
+            results.append(Interpolate.cubic_inter(chunks[i], distance_threshold))
+
+        return pd.concat(results).reset_index().set_index([const.TRAJECTORY_ID, const.DateTime]).sort_values([const.TRAJECTORY_ID, const.DateTime])
+        #return NumTrajDF(pd.concat(results).reset_index(), const.LAT, const.LONG, const.DateTime, const.TRAJECTORY_ID)
+
+    @staticmethod
+    def cubic_inter(dataframe, distance_threshold):
+        df = dataframe.copy().set_index([const.DateTime])
+        idx = df[const.TRAJECTORY_ID].iloc[0]
+        if len(dataframe) > 6:
+            for i in range(len(dataframe)-7):
+                seven_points = df.iloc[i:i+7, :]
+                if Interpolate.check_threshold(seven_points, distance_threshold):
+                    vals = ip_help.cubic(seven_points)
+                    df.loc[pd.to_datetime(vals['inter_time'])] = [idx, vals['inter_x'], vals['inter_y'], 0]
+
+
+        return df
+
+    @staticmethod
+    def check_threshold(df, threshold):
+        dists = df['Distance_prev_to_curr'].to_list()
+        flag = False
+        for i in range(len(dists)):
+            if dists[i] > threshold:
+                flag = True
+
+        return flag
+
+    @staticmethod
+    def calculate_error(df, f1=None, rang=(0, 0), window_size=7):
+        start = rang[0]
+        if rang[1] == 0:
+            limit = df.shape[0]
+        else:
+            limit = rang[1]
+        end = limit
+        ln = int(window_size / 2)
+        da = [0] * ln
+        print(window_size, "dd", da)
+        for ix in range(end - start - window_size):
+            try:
+                seven_points = df.iloc[start + ix:start + ix + window_size, :]
+                lat = seven_points.lat.values
+                lon = seven_points.lon.values
+                p1, p2, pc, d = f1(seven_points)
+            except:
+                d = 0
+            da.append(d)
+
+        for i in range(ln + 1):
+            da.append(0)
+        print(window_size, "dd", da[-10:])
+        return da
 
     @staticmethod
     def kinetic_interpolation(dataframe):
