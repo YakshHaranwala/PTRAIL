382fd02f46a20292e713c5dac6706d49debe4849
YakshHaranwala <yjharanwala@mun.ca>
2021-07-15 11:41:44 -0400

    Released 0.1.2 Beta version of NumMobility on PyPI. Also added ReadMe. Not very complete right now but will add more things as we go.

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ REQUIRED_PKGS = ['numpy',
 setup(
     name='Nummobility',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2 Beta',
+
     license='new BSD',
     python_requires='>=3.6',
     description='A Mobility-data Preprocessing Library using parallel computation.',
