22b0bf5c3480df040a9f447b7c5b6050b0eb2f8c
YakshHaranwala <yjharanwala@mun.ca>
2021-08-03 18:11:34 -0230

    Added a new idea for points of the trajectory for which we can find the POIs in a 1000 m range.

--- Nummobility/semantics/semantic_features.py
+++ Nummobility/semantics/semantic_features.py
@@ -18,6 +18,8 @@ import pandas as pd
 
 from Nummobility.core.TrajectoryDF import NumPandasTraj
 from Nummobility.features.spatial_features import SpatialFeatures
+from Nummobility.utilities.DistanceCalculator import FormulaLog
+
 import Nummobility.utilities.constants as const
 
 
@@ -34,14 +36,10 @@ class SemanticFeatures:
         lst = [start]
         # First, we generate the distance of all the points of the trajectory from the
         # first point of the trajectory and then lets divide it in n equal parts.
-        df = SpatialFeatures.create_distance_from_given_point_column(dataframe=dataframe,
-                                                                     coordinates=start)
-
-        len_traj = SpatialFeatures.get_distance_travelled_by_traj_id(dataframe,
-                                                                     dataframe.reset_index()[const.TRAJECTORY_ID][0])
+        df = SpatialFeatures.create_distance_from_start_column(dataframe)
 
         for i in range(len(df)):
-            if df[f'Distance_from_{start}'][i] > dist_threshold:
+            if df[f'Distance_start_to_curr'][i] > dist_threshold:
                 pt = dataframe[const.LAT][i], dataframe[const.LONG][i]
                 lst.append(pt)
                 dist_threshold += dist_threshold
@@ -50,8 +48,23 @@ class SemanticFeatures:
         return lst
 
     @staticmethod
-    def small_dist(dataframe, start):
-        pass
+    def nearest_bank_alt(df: NumPandasTraj, dist=1000):
+        start = df[const.LAT][0], df[const.LONG][0]
+        dead_start = df[const.LAT][0], df[const.LONG][0]
+        lst = list()
+
+        for i in range(1, len(df)):
+            pt = df[const.LAT][i], df[const.LONG][i]
+            if FormulaLog.haversine_distance(start[0], start[1], pt[0], pt[1]) > dist:
+                if FormulaLog.haversine_distance(dead_start[0], dead_start[1], pt[0], pt[1]) > dist:
+                    lst.append(pt)
+                    start = pt
+
+        tags = {'amenity': ['atm', 'banks']}
+
+
+
+        return lst
 
     @staticmethod
     def distance_from_nearby_hotels():
