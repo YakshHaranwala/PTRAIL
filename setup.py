1a8f086cfc33b3ab73e384a4b2432d988cd33c34
YakshHaranwala <yjharanwala@mun.ca>
2021-08-13 16:22:21 -0230

    Revamped the semantic features notebook. Also solved an error in semantic_features.py.

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ from setuptools import setup
 with open('README.md', 'r') as f:
     LONG_DESCRIPTION = f.read()
 
-REQUIRED_PKGS = ['numpy',
-                 'hampel',
-                 'pandas',
-                 'scipy',
-                 'psutil',
-                 'folium',
-                 'matplotlib',
-                 'scikit-learn',
-                 'osmnx',
-                 'geopandas',
-                 'shapely'
+REQUIRED_PKGS = ['numpy>=1.20',
+                 'hampel >= 0.0.5',
+                 'pandas >= 1.2.5',
+                 'scipy >= 1.6.2',
+                 'psutil >= 5.8.0',
+                 'folium >= 0.12',
+                 'matplotlib >= 3.3.4',
+                 'scikit-learn >= 0.24.2',
+                 'osmnx >= 1.1.1',
+                 'geopandas >= 0.8.1',
+                 'shapely >= 1.7.1'
                  ]
 
 setup(
     name='Nummobility',
     packages=find_packages(),
-    version='0.2.3 Beta',
+    version='0.3 Beta',
     license='new BSD',
     python_requires='>=3.6',
     description='A Mobility-data Preprocessing Library using parallel computation.',
