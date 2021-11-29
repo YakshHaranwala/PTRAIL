f6ef715f187ca2f605aae4a0764ee157142ef4c1
YakshHaranwala <yjharanwala@mun.ca>
2021-11-29 15:27:51 -0330

    Merged the bar chart and radar chart together. Please view it in full screen for now.

--- ptrail/visualization/HydrationTrends.py
+++ ptrail/visualization/HydrationTrends.py
@@ -16,20 +16,20 @@
 """
 import pandas as pd
 from IPython.core.display import display
-from ipywidgets import widgets
+from ipywidgets import widgets, AppLayout
 from ptrail.core.TrajectoryDF import PTRAILDataFrame
+
 from ptrail.features.temporal_features import TemporalFeatures as temp
 from ptrail.preprocessing.filters import Filters as filt
-import matplotlib.pyplot as plt
 
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
 pd.options.mode.chained_assignment = None
 
-sns.set()
-class BarPlot:
+
+class HydrationTrends:
     # Class variables to store datasets and the widget.
     __habitat_data = None
     __traj_data = None
@@ -37,8 +37,8 @@ class BarPlot:
     __list = None
 
     @staticmethod
-    def show_bar_plot(trajectories: PTRAILDataFrame, habitat: pd.DataFrame,
-                                dist_from_water: int):
+    def show_hydration_trends(trajectories: PTRAILDataFrame, habitat: pd.DataFrame,
+                              dist_from_water: int):
         """
             Plot the interactive plotly Radar chart that shows the number of days spent
             by animals around a specific water body.
@@ -64,12 +64,12 @@ class BarPlot:
                 None
         """
         # Store the datasets in the class variables.
-        BarPlot.__habitat_data = habitat
-        BarPlot.__traj_data = trajectories
+        HydrationTrends.__habitat_data = habitat
+        HydrationTrends.__traj_data = trajectories
 
         # First, create the date column on the trajectory dataset.
-        BarPlot.__traj_data = temp.create_date_column(BarPlot.__traj_data)
-        BarPlot.__traj_data = temp.create_time_of_day_column(BarPlot.__traj_data)
+        HydrationTrends.__traj_data = temp.create_date_column(HydrationTrends.__traj_data)
+        HydrationTrends.__traj_data = temp.create_time_of_day_column(HydrationTrends.__traj_data)
 
         # Now, filter out the moving water bodies.
         a = habitat[(habitat['EcoGener'] == 'WR')]
@@ -89,28 +89,80 @@ class BarPlot:
         for i in range(len(water_bodies)):
             point_dict[f'WaterBody #{i + 1}'] = water_bodies.iloc[i]
 
-        BarPlot.__point_dict = point_dict
+        HydrationTrends.__point_dict = point_dict
 
         # Create the dropdown widget.
-        BarPlot.__list = widgets.Dropdown(options=list(point_dict.keys()),
-                                                   value='WaterBody #1',
-                                                   description='WaterBody: ',
-                                                   disabled=False, continuous_update=False)
+        HydrationTrends.__list = widgets.Dropdown(options=list(point_dict.keys()),
+                                                  value='WaterBody #1',
+                                                  description='WaterBody: ',
+                                                  disabled=False, continuous_update=False)
 
         # Show the plot.
-        ie = widgets.interactive_output(BarPlot.__plot_bar,
-                                        {'body_name': BarPlot.__list})
+        radar = widgets.interactive_output(HydrationTrends.__plot_radar,
+                                           {'body_name': HydrationTrends.__list})
+
+        bar = widgets.interactive_output(HydrationTrends.__plot_bar,
+                                         {'body_name': HydrationTrends.__list})
+
+        output = AppLayout(header=None, center=None, footer=None,
+                           left_sidebar=bar, right_sidebar=radar,
+                           pane_heights=[1, 1, '480px'])
 
         # Display the widget and its output side-by-side.
-        display(BarPlot.__list, ie)
+        display(HydrationTrends.__list, output)
+
+    @staticmethod
+    def __plot_radar(body_name):
+        # Get the water body data by its name.
+        point = HydrationTrends.__point_dict[body_name]
+
+        # Filter the trajectory dataset by the water body.
+        dataset = filt.filter_by_bounding_box(HydrationTrends.__traj_data, point['bbox'])
+
+        small_df = pd.DataFrame(columns=['traj_id', 'days', 'Species']).set_index('traj_id')
+        ids_ = dataset.reset_index()['traj_id'].unique()
+        for j in range(len(ids_)):
+            mini = dataset.reset_index().loc[dataset.reset_index()['traj_id'] == ids_[j]]
+            if 'D' in ids_[j]:
+                small_df.loc[ids_[j]] = [pd.to_datetime(mini.Date).nunique(), 'Deer']
+            elif 'E' in ids_[j]:
+                small_df.loc[ids_[j]] = [pd.to_datetime(mini.Date).nunique(), 'Elk']
+            else:
+                small_df.loc[ids_[j]] = [pd.to_datetime(mini.Date).nunique(), 'Cattle']
+
+        fig1 = px.scatter_polar(small_df, r='days', color='Species',
+                                title=f'Number of Days Spent Around {body_name}',
+                                template="ggplot2", color_discrete_sequence=px.colors.qualitative.Vivid, )
+
+        fig1.update_layout(
+            polar=dict(
+                radialaxis=dict(visible=True),
+            ),
+            title=dict(
+                font={'size': 13}
+            ),
+            margin=dict(l=20, r=20, t=30, b=20),
+            autosize=False,
+            width=540,
+            height=280,
+            paper_bgcolor="Gainsboro",
+        )
+
+        fig1.update_polars(
+            angularaxis_tickmode='array',
+            angularaxis_tickvals=[0, 90, 180, 270],
+            angularaxis_ticktext=['', '', '', ''],
+        )
+
+        fig1.show()
 
     @staticmethod
     def __plot_bar(body_name):
         # Get the water body data by its name.
-        point = BarPlot.__point_dict[body_name]
+        point = HydrationTrends.__point_dict[body_name]
 
         # Filter the trajectory dataset by the water body.
-        dataset = filt.filter_by_bounding_box(BarPlot.__traj_data, point['bbox'])
+        dataset = filt.filter_by_bounding_box(HydrationTrends.__traj_data, point['bbox'])
 
         new_species = []
         for i in range(len(dataset)):
@@ -123,23 +175,30 @@ class BarPlot:
 
         dataset['Species'] = new_species
 
+        # Order the dataset according to the times of day.
         small_df = dataset.groupby(by=['Time_Of_Day', 'Species']).count().reset_index()
+        times = ['Early Morning', 'Morning', 'Noon', 'Evening', 'Night', 'Late Night']
+        small_df['Time_Of_Day'] = pd.Categorical(small_df['Time_Of_Day'],
+                                                 categories=times,
+                                                 ordered=True)
+        small_df = small_df.sort_values(by='Time_Of_Day', ascending=True)
 
+        # Plot the barplot from the data above.
         fig, ax = plt.subplots(figsize=(7, 5))
         sns.barplot(x='Time_Of_Day', y='lat', hue='Species', hue_order=['Deer', 'Elk', 'Cattle'], data=small_df, ax=ax)
 
-        fig.text(1.1, 0.5,
+        # Set the description of the text.
+        fig.text(-0.4, 0.5,
                  f"{body_name} Description\n\n"
-                 f"Name: {point['CowPast']}\n"
-                 f"Coordinates: {round(point['lat'], 2), round(point['lon'],2)} \n"
+                 f"Pasture Name: {point['CowPast']}\n"
+                 f"Coordinates: {round(point['lat'], 2), round(point['lon'], 2)} \n"
                  f"Elevation: {point['Elev']} m \n"
                  f"Canopy Cover: {point['Canopy']}%"
-                 )
+                 , fontdict={'size': 13})
 
-        # fig.set_facecolor('black')
+        fig.set_facecolor('lightgray')
         ax.set_xlabel('TIME OF THE DAY')
         ax.set_ylabel('NUMBER OS TIMES VISITED')
         ax.set_title('Hydration trend of Species throughout the day')
+
         fig.tight_layout()
-
-
